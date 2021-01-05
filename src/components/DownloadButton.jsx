76d7663abacf15883c4d385f12d9d30a00f63d6c
andy <andersgee@gmail.com>
2021-01-05 12:18:38 +0100

    Downloadbutton

--- src/components/DownloadButton.jsx
+++ src/components/DownloadButton.jsx
@@ -3,11 +3,9 @@ import { Button, Tooltip } from "@material-ui/core";
 import SaveIcon from "@material-ui/icons/Save";
 
 export default function DownloadButton(props) {
-  const aref = useRef();
   const handleClick = () => {
     const canvas = document.getElementsByTagName("canvas")[0];
-    const link = aref.current;
-    //const link = document.getElementById("canvasdownloadlink");
+    const link = document.createElement("a");
     link.setAttribute("download", "TemperatureContributionFigure.png");
     link.setAttribute(
       "href",
@@ -18,9 +16,6 @@ export default function DownloadButton(props) {
 
   return (
     <>
-      <a ref={aref} display="none" href="/Emissions.xlsx">
-        canvasdownloadlink
-      </a>
       <Tooltip title="Save Image" aria-label="save image">
         <Button onClick={handleClick} color="primary" variant="contained">
           <SaveIcon />
